647b71c4a74c4d93acc05c0b8cee37cbaab218f0
jdorta08 <dortajavier.jd2@gmail.com>
2023-04-28 19:08:24 -0400

    changes to css and html

---
+++ js/validation.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("#contact-form");
+const nameInput = document.getElementById("#name");
+const emailInput = document.getElementById("#email");
+const messageInput = document.getElementById("#message");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  validateForm();
+});
+
+function validateForm() {
+  if (!nameInput.value.trim()) {
+    setError(nameInput, "Please enter your name");
+  } else if (nameInput.value.trim().lenght() < 5) {
+    setError(nameInput, "Your full name must be at least 5 characters long");
+  }
+  if (!emailInput.value.trim()) {
+    setError(emailInput, "Please enter your email");
+  } else if (!validateEmail(emailInput.value)) {
+    setError(emailInput, "Please enter a valid email address");
+  }
+  if (!messageInput.value.trim()) {
+    setError(messageInput, "Please enter your message");
+  }
+}
+
+function setError(input, message) {
+  const formControl = input.parentElement;
+  formControl.classList.add("error");
+  const paragraph = formControl.querySelector("p");
+  paragraph.textContent = message;
+}
+
+function validateEmail(emailInput) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return mailformat.test(emailInput);
+}
