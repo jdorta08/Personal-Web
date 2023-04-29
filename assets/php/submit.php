b49326b7a38183be4a798d69b661b49fb2d33e92
jdorta08 <dortajavier.jd2@gmail.com>
2023-04-28 21:15:02 -0400

    php submit

--- assets/php/submit.php
+++ assets/php/submit.php
@@ -1,12 +1,27 @@
 <?php
+require 'phpmailer/PHPMailerAutoload.php';
 
-if ($SERVER['REQUEST_METHOD'] == 'POST'){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-}
-
-$to = "jdweb@javiwebs.com";
-$subject = "New form submission from JaviWebs";
-$body = "Name: $name\nEmail: $email\nMessage: $message";
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+  
+    // Validate form data
+    if (empty($name) || empty($email) || empty($message)) {
+      echo "Please fill in all required fields";
+    } else {
+      // Send email
+      $to = "jdweb@javiwebs.com";
+      $subject = "New form submission JaviWebs";
+      $body = "Name: $name\nEmail: $email\nMessage: $message";
+  
+      if (mail($to, $subject, $body)) {
+        echo "Thank you for your submission!";
+      } else {
+        echo "There was an error sending your message. Please try again later.";
+      }
+    }
+  }
+  
+  ?>
+  
